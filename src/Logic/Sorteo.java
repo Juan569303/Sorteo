be79bc55d9efec493aedb3c3ab0ad5387a560929
Juan Zapata <juanzapata_24@outlook.com>
2025-07-04 02:42:47 -0500

    Creación de la interfaz y parte de la logica

---
+++ src/Logic/Sorteo.java
@@ -0,0 +1,17 @@
+package Logic;
+
+import gui.Main;
+import java.awt.Color;
+
+public class Sorteo {
+
+    public static void main(String[] args) {
+
+        Main window = new Main();
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+       
+        
+    }
+    
+}
